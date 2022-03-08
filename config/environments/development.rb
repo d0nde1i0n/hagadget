3e0d3316fe11d0921778138c8284619f9ced7941
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-08 06:16:13 +0000

    [Fix] config.hostsの値を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "758fb635c6f84c749bcbed9615cb8b30.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
