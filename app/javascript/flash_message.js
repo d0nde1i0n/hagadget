eefa522acd5a0fb6f89f511a114eab0c28aba6aa
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-08 12:42:52 +0000

    [Update]　フラッシュメッセージのレイアウト、動的処理の更新

---
+++ app/javascript/flash_message.js
@@ -0,0 +1,9 @@
+//処理成功時に表示させるフラッシュメッセージの処理
+$(function(){
+  setTimeout("$('.success').fadeOut('nomal')", 2000);
+});
+
+// 処理失敗時に表示させるフラッシュメッセージの処理
+$(function(){
+  setTimeout("$('.error').fadeOut('nomal')", 2000);
+});
