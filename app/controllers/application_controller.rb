555a9a34c969caac62828f613155632d7fadf13b
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-09 12:32:15 +0000

    [Add] 新規ユーザ登録画面の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # デバイスコントローラが使用される前の処理を追記
+  before_action :configure_permitted_parameters,if: :devise_controller?
+
+
+  protected
+
+  # メールアドレス、パスワード以外のカラムも扱えるようにストロングパラメータを設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:last_name,:first_name,:nickname,:occupation_id])
+  end
+
 end
--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -1,6 +1,6 @@
 class Occupation < ApplicationRecord
 
-  has_many users, dependent: :destroy
+  has_many :users, dependent: :destroy
   validates :name,presence: true
 
 end
