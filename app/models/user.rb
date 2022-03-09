7fac51dde237d047e80f05bbb4d4fc459bf0ff2d
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-09 11:17:00 +0000

    [Start]　ユーザ関連機能の実装開始

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,16 @@ class User < ApplicationRecord
 
   # アソシエーション(関連付け)
   belongs_to :occupation
+  has_one_attached :profile_image
 
   # バリデーション（検証）
   validates :last_name,:first_name,:nickname,length: {minimum: 1, maximum: 10}
   validates :introduction,length: {maximum: 200}
+
+  # メソッド
+  # ファイルが添付されているかを確認し、添付されていない場合には指定のファイルを添付するメソッド
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_profile_image'
+  end
 
 end
