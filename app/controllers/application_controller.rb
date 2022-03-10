15968aea1ea7a01c405f565830a863ac21e0baef
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-10 10:16:18 +0000

    [Add] ログイン後の遷移先を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   # デバイスコントローラが使用される前の処理を追記
   before_action :configure_permitted_parameters,if: :devise_controller?
-  
+
   # device関連メッセージの日本語化
   before_action do
     I18n.locale = :ja # 〇〇.ja.ymlを指定している？
   end
-  
+
+  # サインアウト（ログアウト）後の遷移先を指定
+  def after_sign_in_path_for(resourse)
+    user_path(current_user) #ログインユーザのユーザ詳細画面
+  end
+
   # サインアウト（ログアウト）後の遷移先を指定
   def after_sign_out_path_for(resourse)
     root_path #トップページ
