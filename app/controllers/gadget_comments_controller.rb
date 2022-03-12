1d2fac13cd14ed470eef76ea0acb93eb523fd7fa
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-12 12:20:50 +0000

    [Add] コメント機能関連のコントローラを作成

---
+++ app/controllers/gadget_comments_controller.rb
@@ -0,0 +1,32 @@
+class GadgetCommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_gadget_inffo
+
+  def create
+    # before_action :set_gadget_infoで「@gadget」を取得
+    # ログインユーザに紐づくコメントを変数に格納
+    @comment = current_user.gadget_comments.new(gadget_comment_params)
+    # ガジェット記事のidを変数に格納
+    @comment.gadget_id = @gadget.id
+    @comment.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    # before_action :set_gadget_infoで「@gadget」を取得
+    # ガジェットコメントid、ガジェット記事idに両方の条件を満たすレコードを検索し、
+    # 一致するレコードを削除
+    GadgetComment.find_by(id: params[:id],gadget_id: @gadget.id).destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def gadget_comment_params
+    params.require(:gadget_comment).permit(:comment)
+  end
+
+  def set_gadget_comment_info
+    @gadget = Gadget.find(params[:gadget_id])
+  end
+end
