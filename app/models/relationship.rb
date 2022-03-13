8a3905bfd49fabd31a39d45ad2a3416fc12461f2
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-13 07:44:59 +0000

    [Fix]　コメント内容の修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,6 +3,6 @@ class Relationship < ApplicationRecord
   # アソシエーション（関連付け）
   # フォローする側のUserテーブルとのアソシエーション
   belongs_to :follower,class_name: 'User'
-  # フォローする側のUserテーブルとのアソシエーション
+  # フォローされる側のUserテーブルとのアソシエーション
   belongs_to :followed,class_name: 'User'
 end
