cf367231231da8f296adefe043e0157134638f91
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-13 07:15:19 +0000

    [Add] フォロー済みかを判定するメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,8 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_profile_image'
   end
 
+  def followerd_by(user)?
+    relationships.where(follower_id: user.id).exists?
+  end
+
 end
