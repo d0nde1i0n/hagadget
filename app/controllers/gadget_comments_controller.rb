9ce982af9515dac364e9a741b42e5429b31f3a72
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-13 14:48:55 +0000

    [Add] コメント投稿/コメント削除後の非同期処理を実装

--- app/controllers/gadget_comments_controller.rb
+++ app/controllers/gadget_comments_controller.rb
@@ -1,32 +1,42 @@
 class GadgetCommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_gadget_info
+  before_action :set_gadget_info, only: [:create,:destroy,:set_gadget_comments_info]
+  before_action :set_gadget_comments_info, only: [:create,:destroy]
 
   def create
     # before_action :set_gadget_infoで「@gadget」を取得
+    # before_action :set_gadget_infoで「@gadget_comments」を取得
     # ログインユーザに紐づくコメントを変数に格納
-    @comment = current_user.gadget_comments.new(gadget_comment_params)
+    @gadget_comment = current_user.gadget_comments.new(gadget_comment_params)
     # ガジェット記事のidを変数に格納
-    @comment.gadget_id = @gadget.id
-    @comment.save
-    redirect_to request.referer
+    @gadget_comment.gadget_id = @gadget.id
+    @gadget_comment.save
+
   end
 
   def destroy
     # before_action :set_gadget_infoで「@gadget」を取得
+    # before_action :set_gadget_infoで「@gadget_comments」を取得
     # ガジェットコメントid、ガジェット記事idに両方の条件を満たすレコードを検索し、
     # 一致するレコードを削除
-    GadgetComment.find_by(id: params[:id],gadget_id: @gadget.id).destroy
-    redirect_to request.referer
+    @gadget_comment = GadgetComment.find_by(id: params[:id],gadget_id: @gadget.id).destroy
+    @gadget_comment.destroy
   end
 
   private
 
+  # ストロングパラメータ
   def gadget_comment_params
     params.require(:gadget_comment).permit(:comment)
   end
 
+  # ガジェット記事を取得するメソッド
   def set_gadget_info
     @gadget = Gadget.find(params[:gadget_id])
+  end
+
+  # ガジェット記事に紐づく、全てのコメントを取得するためのメソッド
+  def set_gadget_comments_info
+    @gadget_comments = @gadget.gadget_comments.all
   end
 end
