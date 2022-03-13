9fea18b1666b9292e21de87b458a21ac8ade4ff8
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-13 06:08:10 +0000

    [Add] Relationships関連のルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
 
   # ユーザ関連のルーティング
-  resources :users, only: [:show,:edit,:update]
+  resources :users, only: [:show,:edit,:update] do
+    resources :relationships,only: [:create,:destroy]
+    # 「on: :member」を使用することで「users/:id/followers」というルーティングが使用可能
+    # 「on: :collection」を使用すると「users/followers」というルーティングになる
+    # ユーザがフォローしているユーザを全てを取得
+    get :followers,on: :member
+    # ユーザをフォローしているユーザを全てを取得
+    get :followings,on: :member
+    #
+  end
+
   # ガジェット記事関連のルーティング
   resources :gadgets, only: [:new,:create,:index,:show,:edit,:update,:destroy] do
     resource :favorites,only: [:create,:destroy]
