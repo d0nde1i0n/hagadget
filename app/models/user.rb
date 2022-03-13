11490497b5c236121af73c5c55d995eceb0f64bc
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-13 05:49:56 +0000

    [Add] Relationshipモデルの追加、アソシエーションについて追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,18 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :gadget_comments,dependent: :destroy
 
+  # 被フォロー側のアソシエーション
+  # UserテーブルとRelationshipテーブルのアソシエーションだが、与フォロー側のアソシエーションと
+  # 区別するために名称「reverse_of_relationship」を新たに付与している。
+  has_many :reverse_of_relationship,class_name: 'Relationship',foregin_key: :followed_id,dependent: :destroy
+  # 与フォロー側のアソシエーション
+  has_many :relationships,foregin_key: :follower_id,dependent: :destroy
+  # 被フォロー関係を通じて、自分をフォローする人を参照するためのアソシエーション
+  has_many :follwers,through: :reverse_of_relationship, source: :follower
+  # 与フォロー関係を通じて、自分がフォローする人を参照するためのアソシエーション
+  has_many :followings,through: :relationship, source: :followed
+
+
   # バリデーション（検証）
   validates :last_name,:first_name,:nickname,length: {minimum: 1, maximum: 10}
   validates :introduction,length: {maximum: 200}
