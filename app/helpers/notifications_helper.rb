60265669fa7f6323a6a6539639ef58dc7c44900f
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-14 14:54:25 +0000

    [Fix] ヘッダー&フッターの不具合修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,7 +2,7 @@ module NotificationsHelper
 
   # 未読の通知が存在するかを確認するヘルパーメソッド
   def unchecked_notifications
-    @notifications = current_user.passive_notication.where(checked: false)
+    @notifications = current_user.passive_notifications.where(checked: false)
   end
 
 end
