f0189fe3092a474316ae0d1ad884c8ca8d28bc44
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-14 08:03:10 +0000

    [Add] 通知機能関連のルーティングを追記

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
   # ユーザ関連のルーティング
   resources :users, only: [:show,:edit,:update] do
     resources :relationships,only: [:create,:destroy]
+    resources :notifications,only: [:index]
+
     # 「on: :member」を使用することで「users/:id/followers」というルーティングが使用可能
     # 「on: :collection」を使用すると「users/followers」というルーティングになる
     # ユーザがフォローしているユーザを全てを取得
     get :followers,on: :member
     # ユーザをフォローしているユーザを全てを取得
     get :followings,on: :member
-    #
   end
 
   # ガジェット記事関連のルーティング
