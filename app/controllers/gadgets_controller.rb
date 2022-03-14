2a5e083c45c4ae716d2bac7de6396afcbc406926
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-14 07:35:44 +0000

    [Add] お気に入り登録後の通知作成メソッドをコントローラに追記

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -11,6 +11,8 @@ class GadgetsController < ApplicationController
     @gadget = current_user.gadgets.new(gadget_params)
 
     if @gadget.save
+      # お気に入り登録に関連する通知レコードをデータベースに登録
+      gadget.create_notification_like!(current_user)
       flash[:notice] = "ガジェット記事を投稿しました。"
       redirect_to gadgets_path
     else
@@ -64,12 +66,41 @@ class GadgetsController < ApplicationController
     @gadget = Gadget.find(params[:id])
   end
 
-   def ensure_correct_user
+  def ensure_correct_user
     # before_actionで「@user」を取得
     # ログインユーザとユーザ詳細画面のユーザが一致しない場合は、ログインユーザのページに遷移
     unless @gadget.user == current_user
       flash[:alert] = "不正な操作です。"
       redirect_to user_path(current_user)
+    end
+  end
+
+   # お気に入り登録された後に通知を作成するメソッド
+  def create_notification_like!(temp_current_user)
+
+    # notificationsテーブルからwhere内の条件に一致するレコードを検索し、tempに格納
+    temp = Notification.where(
+      ["visitor_id = ? and visited_id = ? and gadget_id = ? and action = ?",
+      temp_current_user.id, user_id, id, Notification.action_types[:liked_to_own_post]
+      ])
+
+    # notificationsテーブルに該当するレコードがない場合のみ、通知レコードを作成
+    if temp.blank
+      # 「.blank?」：対象オブジェクトが空白の場合にtrueを返すメソッド
+      # ここでいう「空白」とは、「空文字」、「空白」、「false」、「nil」を指す。
+
+      # Notificationクラスの空のインスタンスを作成後、各カラムに値を追加
+      notification = temp_current_user.active_notifications.new(
+        gadget_id: id,visited_id: user_id, action: Notification.action_types[:liked_to_own_post]
+        )
+
+      # ユーザAが投稿したガジェット記事に対して、ユーザA自身がお気に入り登録した場合、通知済みとする
+      # (上記の場合に、通知が送信されるのを防止するため)
+      notification.checked = true if notification.visitor_id == notification.visited_id
+
+      # バリデーションエラーがない場合のみ、データベースに通知レコードを登録する。
+      notification.save if notification.valid?
+
     end
   end
 end
