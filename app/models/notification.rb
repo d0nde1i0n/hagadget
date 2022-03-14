5354c042d858d99cc6eabd04956cce3dbde7af11
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-14 06:11:19 +0000

    [Add] 通知機能に関連するモデルファイルにアソシエーション、バリデーションを追記

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,18 @@
 class Notification < ApplicationRecord
-  belongs_to :visitor
-  belongs_to :visited
-  belongs_to :gadget
-  belongs_to :gadget_comment
+
+  # 通知が作成日の降順に並ぶようにデフォルト設定を変更
+  default_scope -> {order(created_at: :desc)}
+
+  # アソシエーション（関連付け）
+  belongs_to :visitor,class_name: 'User'
+  belongs_to :visited,class_name: 'User'
+   # 「optional: true」：格納される値にnullを許可する。
+  # （通常外部キーとなるカラムの値にnullが許可されていないため）
+  belongs_to :gadget,optional: true
+  belongs_to :gadget_comment,optional: true
+
+  # バリデーション検証
+  validates :action,presence: true
+  # 「inclusion: {in: 〇〇}」：保存できる値を〇〇に制限する。
+  validates :checked, inclusion: {in: [true,false]}
 end
