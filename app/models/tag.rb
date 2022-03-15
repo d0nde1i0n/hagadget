ae9dc59a8af03da9b1a34aecd451e1c7c909ab80
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-15 08:05:58 +0000

    [Fix] バリデーションのオプションの記載内容を修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
 
   # アソシエーション（関連付け）
-  has_many :gadget_tags,destroy: :destroy
+  has_many :gadget_tags,dependent: :destroy
   has_many :gadgets,through: :gadget_tags
 
   # バリデーション（検証）
