2fb60e410f185ff60473a27674cbc50e2444c654
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-15 05:46:06 +0000

    [Add] タグ機能を実装する上で必要なアソシエーション、バリデーションを追記

--- app/models/gadget_tag.rb
+++ app/models/gadget_tag.rb
@@ -1,4 +1,6 @@
 class GadgetTag < ApplicationRecord
+  
+  # アソシエーション（関連付け）
   belongs_to :gadget
   belongs_to :tag
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,10 @@
 class Tag < ApplicationRecord
+
+  # アソシエーション（関連付け）
+  has_many :gadget_tags,destroy: :destroy
+  has_many :gadgets,through: :gadget_tags
+
+  # バリデーション（検証）
+  validates :name,length: {minimum: 1,maximum: 15}
+
 end
