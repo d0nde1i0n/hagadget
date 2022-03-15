fac4662bd994985a035d9959b0423717fc56cc20
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-15 12:26:51 +0000

    [Add] ユーザ、ガジェットの検索機能を追加（タグの検索機能は未実装）

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   # トップページへのルーティング
   root :to => 'homes#top'
   # 検索結果ページのルーティング
-  get '/search' => "searches#search"
 
   # ユーザ関連のルーティング
   resources :users, only: [:show,:edit,:update] do
@@ -24,5 +23,7 @@ Rails.application.routes.draw do
     resource :favorites,only: [:create,:destroy]
     resources :gadget_comments,only: [:create,:destroy]
   end
+
+  get "/search" => "searches#search"
 
 end
