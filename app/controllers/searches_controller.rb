15476fb5e30445ac14da4711bb39dd3c3b1fc5b4
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-15 12:26:51 +0000

    [Add] ユーザ、ガジェットの検索機能を追加（タグの検索機能は未実装）

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,25 +1,26 @@
 class SearchesController < ApplicationController
 
   def search
-    @model = params["model"] #検索対象のモデルを格納
-    @content = params["content"] #検索対象の文字列を格納
+    @model = params[:model] #検索対象のモデルを格納
+    @content = params[:content] #検索対象の文字列を格納
     # 「search_for()メソッドで取得したレコードを全て格納
-    @records = search_for(@model,@content)
+    @records = search_for(@model,@content).page(params[:page])
   end
 
   private
 
+  # テーブルから検索条件に一致するレコードを収集するインスタンスメソッド
   def search_for(model,content)
 
     # 受信したモデル情報別に処理を分岐
     # 今回の曖昧検索では、部分一致のみとしている。
-    if model == 'User'
-      User.where('name LIKE ?','%'+content+'%')
+    if model == 'user'
+      User.where('nickname LIKE ?','%'+content+'%')
 
-    elsif model == 'Gadget'
+    elsif model == 'gadget'
       Gadget.where('name LIKE ?','%'+content+'%')
 
-    elsif model == 'Tag'
+    elsif model == 'tag'
       Tag.where('name LIKE ?','%'+content+'%')
     end
 
