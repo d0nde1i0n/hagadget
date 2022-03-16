c1a8b73eca65c02ffc6bd23da89141cdb93b537a
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-16 10:35:19 +0000

    [Update] 検索可能なカテゴリに「タグ名」を追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,19 +11,23 @@ class SearchesController < ApplicationController
 
   # テーブルから検索条件に一致するレコードを収集するインスタンスメソッド
   def search_for(model,content)
+    # モデル別に処理を分岐、あいまい検索(LIKE)条件は「部分一致」のみとしている。
 
-    # 受信したモデル情報別に処理を分岐
-    # 今回の曖昧検索では、部分一致のみとしている。
+    # ユーザの場合
     if model == 'user'
       User.where('nickname LIKE ?','%'+content+'%')
-
+    # ガジェット（ガジェット名）の場合
     elsif model == 'gadget'
       Gadget.where('name LIKE ?','%'+content+'%')
-
+    # ガジェット（タグ名）の場合
     elsif model == 'tag'
-      Tag.where('name LIKE ?','%'+content+'%')
+      tags = Tag.where('name LIKE ?','%'+content+'%')
+      # 検索条件のタグ名に紐づくガジェット記事を、配列「gadget_articles」に格納
+      # 「ingect」：たたみ込み演算を行うメソッド（eachメソッドよりもコードが短くなる）
+      gadget_articles = tags.inject(init = []) {|result, tag| result + tag.gadgets}
+      # 「page」メソッドで配列扱えるように「Kaminari.pagenate_array」を付加して返す
+      return Kaminari.paginate_array(gadget_articles)
     end
-
   end
 
 end
