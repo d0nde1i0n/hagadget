1d8bd7e20436beb3cfd1af25d3dea040d90b36c4
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-17 11:54:09 +0000

    [Add] 投稿ガジェット一覧に対するソート機能を実装

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -3,6 +3,8 @@ class GadgetsController < ApplicationController
   before_action :set_gadget_info,only:[:show,:edit,:update,:destroy]
   before_action :ensure_correct_user,only: [:edit,:update,:destroy]
   before_action :set_tag_list,only: [:create,:update]
+  helper_method :sort_column,:sort_direction
+
 
   def new
     @gadget = Gadget.new
@@ -84,4 +86,19 @@ class GadgetsController < ApplicationController
     @tag_list = params[:gadget][:tag_name].split(nil)
   end
 
+   # データ並べ替え手段の指定値を確認するメソッド
+  def sort_direction
+    # 受け取った値が指定する値かを判定する
+    # （「asc」、「desc」であればそのまま、その他の値を受け取った場合は「asc」を返す）
+    # 「%w」:文字列からなる配列を作成したいときに「[ ]」,「" "」を省略して記述するためのRubyの構文。
+    # 「include(指定値)」: 配列の要素に”指定値”が含まれているばTrueを返す。
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
+  end
+
+  # データ並べ替え手段の対象となるカラム名を確認するメソッド
+  def sort_column
+    # 受け取った値がGadgetテーブルのカラム名と一致するかを判定する
+    # （一致するカラム名があればそのまま、その他の値を受け取った場合は: 'id'を返す）
+    Gadget.column_names.include?(params[:sort]) ? params[:sort] : 'id'
+  end
 end
