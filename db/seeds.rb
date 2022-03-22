dc08bac18dfd1346bade835e24e5b0eacdd63f0f
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-22 12:42:26 +0000

    [Update] 職業欄に表示する名称を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Occupation.create([
+  {name: 'IT・Web'},
+  {name: '医療'},
+  {name: '福祉'},
+  {name: '美容・ファッション'},
+  {name: '観光'},
+  {name: '教育・保育'},
+  {name: '学生'},
+  {name: 'その他'}
+  ])
