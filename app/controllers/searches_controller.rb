fba181063bd0c1bf3080d9c1760c284c53f3d5b5
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-03-29 07:35:48 +0000

    [Update] 検索結果画面を表示時に発生するN+1問題への対処完了

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -15,24 +15,31 @@ class SearchesController < ApplicationController
 
     # ユーザ（ユーザ名）の場合
     if model == 'user'
-      User.where('nickname LIKE ?','%'+content+'%')
+      # N+1問題への対応
+      User.includes(:occupation,profile_image_attachment: :blob).where('nickname LIKE ?','%'+content+'%')
 
     # ユーザ（職種名）の場合
+      # N+1問題への対応
     elsif model == 'occupation'
       occupation = Occupation.select(:id).where('name LIKE ?',content)
-      User.where(occupation_id: occupation)
+      User.includes(:occupation,profile_image_attachment: :blob).where(occupation_id: occupation)
 
     # ガジェット（ガジェット名）の場合
     elsif model == 'gadget'
-      Gadget.where('name LIKE ?','%'+content+'%')
+      # N+1問題への対応
+      Gadget.includes(:tags,user: {profile_image_attachment: :blob},gadget_image_attachment: :blob).where('name LIKE ?','%'+content+'%')
 
     # ガジェット（タグ名）の場合
     elsif model == 'tag'
-      tags = Tag.where('name LIKE ?',content)
+      p tags = Tag.where('name LIKE ?',content)
       # 検索条件のタグ名に紐づくガジェット記事を、配列「gadget_articles」に格納
       # 「ingect」：たたみ込み演算を行うメソッド（eachメソッドよりもコードが短くなる）
-      gadget_articles = tags.inject(init = []) {|result, tag| result + tag.gadgets}
+      gadget_articles = tags.inject(init = []) {
+        |result, tag| result + tag.gadgets.includes(:tags,user: {profile_image_attachment: :blob},
+        gadget_image_attachment: :blob
+        )}
       # 「page」メソッドで配列扱えるように「Kaminari.pagenate_array」を付加して返す
+      # N+1問題への対応
       return Kaminari.paginate_array(gadget_articles)
     end
   end
